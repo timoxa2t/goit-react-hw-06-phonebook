6adc4fbc31087e93f30bf9a566418cfbed8c8307
timoxa2t <alex2t172@gmail.com>
2022-10-03 17:26:27 +0300

    e

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,4 +1,4 @@
-import { configureStore, createStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import { rootReducer } from 'reducers/phonebook'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
